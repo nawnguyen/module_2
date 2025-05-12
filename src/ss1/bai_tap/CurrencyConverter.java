81b1c17b4f74b62503db3f852ec8ff2fa13f0bb7
nawnguyen <namnv456@gmail.com>
2025-05-12 11:25:15 +0700

    fix input condition

--- src/ss1/bai_tap/CurrencyConverter.java
+++ src/ss1/bai_tap/CurrencyConverter.java
@@ -4,12 +4,25 @@ import java.util.Scanner;
 
 public class CurrencyConverter {
     public static void main(String[] args) {
-        double vnd = 23000;
+        final double RATE = 23000;
         double usd;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap so tien USD: ");
-        usd = scanner.nextDouble();
-        double convert = usd * 23000;
-        System.out.print("Gia tri VND: " + convert);
+        while (true) {
+            System.out.print("Nhap so tien USD: ");
+            if (!scanner.hasNextDouble()) {
+                System.out.println("Đầu vào không hợp lệ! Nhập lại một số");
+                scanner.next();
+            } else {
+                usd = scanner.nextDouble();
+                if (usd < 1 || usd > 1000) {
+                    System.out.println("Đầu vào không hợp lệ! Hãy nhập số trong khoảng 1 đến 1000");
+                } else {
+                    break;
+                }
+            }
+        }
+
+        double convert = usd * RATE;
+        System.out.println("Gia tri VND: " + convert);
     }
 }
