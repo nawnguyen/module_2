04f2d15b32421b38e2d8c06aec4e16e3b42247ca
nawnguyen <namnv456@gmail.com>
2025-05-15 09:00:32 +0700

    fix condition

--- src/ss4/BaiTap/Main.java
+++ src/ss4/BaiTap/Main.java
@@ -5,13 +5,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        boolean checkInput;
+        do{
+            checkInput = true;
 
-        System.out.print("Enter a: ");
-        double a = scanner.nextDouble();
-        System.out.print("Enter b: ");
-        double b = scanner.nextDouble();
-        System.out.print("Enter c: ");
-        double c = scanner.nextDouble();
+            System.out.print("Enter a: ");
+            if (scanner.hasNextDouble()) {
+                a = scanner.nextDouble();
+                if (a == 0) {
+                    System.out.println("'a' must be different from 0 !");
+                    checkInput = false;
+                    continue;
+                }
+            } else {
+                System.out.println("Invalid input !");
+                checkInput = false;
+                scanner.nextLine();
+                continue;
+            }
+            System.out.print("Enter b: ");
+            if (scanner.hasNextDouble()) {
+                b = scanner.nextDouble();
+            } else {
+                System.out.println("Invalid input !");
+                checkInput = false;
+                scanner.nextLine();
+                continue;
+            }
+            System.out.print("Enter c: ");
+            if (scanner.hasNextDouble()) {
+                c = scanner.nextDouble();
+            } else {
+                System.out.println("Invalid input !");
+                checkInput = false;
+                scanner.nextLine();
+                continue;
+            }
+        }while(!checkInput);
+
+
 
         QuadraticEquation equation = new QuadraticEquation(a, b, c);
 
